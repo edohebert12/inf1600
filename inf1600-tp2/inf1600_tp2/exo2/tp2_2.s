07f67e602bd317d081496d9ec01f13cdf997c5dc
Edouard Hebert <edohebert12@gmail.com>
2017-02-23 15:57:07 -0500

    c1

--- inf1600-tp2/inf1600_tp2/exo2/tp2_2.s
+++ inf1600-tp2/inf1600_tp2/exo2/tp2_2.s
@@ -1,6 +1,28 @@
 .global func_s
 
 func_s:
-	/* Votre programme assembleur ici... */
-
+	
+	flds f
+	flds g
+	fsubrp
+	fstps a
+	flds b
+	flds d
+	fmulp
+	flds c
+	fsubrp
+	flds a
+	fdivrp
+	flds e
+	faddp
+	fstps a
+	flds g
+	flds e
+	fsubrp
+	flds f
+	fdivrp
+	flds a
+	fmulp
+	fstps a
+	
 	ret
