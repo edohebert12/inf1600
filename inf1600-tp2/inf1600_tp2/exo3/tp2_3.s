5c88d0cfb2f7b00d75b3955b1456fefd4c3d0317
Edouard Hebert <edohebert12@gmail.com>
2017-02-28 10:53:49 -0500

    commentaires tp2_3.s

--- inf1600-tp2/inf1600_tp2/exo3/tp2_3.s
+++ inf1600-tp2/inf1600_tp2/exo3/tp2_3.s
@@ -10,53 +10,57 @@ func_s:
 	boucle:
         	mov i, %eax
 		mov max, %ebx
-		cmp %ebx, %eax
-		ja fin
+		cmp %ebx, %eax   # comparaison de i et max
+		ja fin           # Si i est plus grand que 10, on termine le programme
 		mov d, %eax
 		mov e, %ebx
 		mov b, %ecx
-		add %ebx, %eax
-		sub %ecx, %eax
-		mov %eax, a
+		add %ebx, %eax   # d + e
+		sub %ecx, %eax   # d + e - b
+		mov %eax, a      # a = d + e - b
 	
-	condition:
+	condition:               # évaluation du premier if
 		mov b, %esi
 		mov c, %edi
-		sub $1000, %esi
-		add $500, %edi
-		add $10000, %esi
-		add $10000, %edi
-		cmp %edi, %esi
-		jae else
+		sub $1000, %esi  # b - 1000
+		add $500, %edi   # c + 500
+		
+		add $10000, %esi # nous avons obtenu des erreurs de signe en testant le programme (ex. 500 n'est pas plus grand que -500)
+		add $10000, %edi # ces instructions servent à conserver des nombres positifs pour les comparaisons
+		
+		cmp %edi, %esi   # (b - 1000) < (c + 500) ?
+		jae else         # Si la condition est fausse, on saute au else
 		mov c, %edx
-		sub $500, %edx
-		mov %edx, c
-		mov b, %ebx
+		sub $500, %edx   # c - 500
+		mov %edx, c      # c = c - 500
+		mov b, %ebx      # préparation de la prochaine condition
 		mov c, %ecx
-		add $10000, %ebx
-		add $10000, %ecx
-		cmp %ecx, %ebx
-		jna repeat
+		
+		add $10000, %ebx # encore une fois, on élimine les possibilités d'erreurs de signe
+		add $10000, %ecx # les valeurs actuelles de b et c ne sont pas modifiées
+		
+		cmp %ecx, %ebx   # b > c ?
+		jna repeat       # Si b est plus petit ou égal à c, on termine cette itération
 		mov b, %ebx
-		sub $500, %ebx
-		mov %ebx, b
-		jmp repeat
+		sub $500, %ebx   # b - 500
+		mov %ebx, b      # b = b - 500
+		jmp repeat       # fin de l'itération
 	
 	else:
 		mov b, %ebx
 		mov e, %ecx
-		sub %ecx, %ebx
-		mov %ebx, b
+		sub %ecx, %ebx   # b - e
+		mov %ebx, b      # b = b - e
 		mov d, %edx
-		add $500, %edx
-		mov %edx, d
-		jmp repeat
+		add $500, %edx   # d + 500
+		mov %edx, d      # d = d + 500
+		jmp repeat       # fin de l'itération
 
 	repeat:
-		mov i, %eax
-		add $1, %eax
-		mov %eax, i
-		jmp boucle
+		mov i, %eax      # à la fin de chaque itération, on passe ici
+		add $1, %eax     # i + 1
+		mov %eax, i      # i = i + 1
+		jmp boucle       # on revient au début
 		
 	fin:
 		ret
