2e8ec50eddeb618c7830e7e6ae46915e6732fc85
edouard.hebert <ucvoic1@gmail.com>
2017-03-19 12:06:18 -0400

    c1

--- tp3/average.s
+++ tp3/average.s
@@ -1,11 +1,74 @@
+.data
+r:
+   .int 0
+c: 
+   .int 0
+temp:
+   .int 0
+
 .global matrix_row_aver_asm
 
 matrix_row_aver_asm:
         push %ebp      			/* Save old base pointer */
         mov %esp, %ebp 			/* Set ebp to current esp */
+        push %esi
+        push %ebx
+        push %edi
 
-		/* Write your solution here */
-		
+        movl 8(%ebp), %esi        # tableau inmatdata
+        movl 12(%ebp), %ebx       # tableau outmatdata
+        movl 16(%ebp), %ecx      # metaorder
+        
+        Boucle1:
+        movl r,%edi
+        cmpl %edi, %ecx          # comparer r avec matorder
+        je  retour
+        mov c,%edx
+        movl $0, %edx           # initialise c pour la prochaine boucle
+        movl %edx, c
+        movl $0, %eax
+        jmp Boucle2
+        
+        continueBoucle1:
+        movl $0, %edx
+        div %ecx
+        movl r,%edi
+        movl %eax, (%ebx,%edi,4)    # Ajouter la valeur de elem dans le tableau outmatdata
+        jmp incrementerR
+        
+
+        Boucle2:
+        movl c,%edx      # comparer c avec matorder
+        cmpl %edx,%ecx
+        je continueBoucle1
+        movl r,%edi
+        imul %ecx, %edi    #mettre la position du nombre a recuperer du tableau inmatdata dans %edx
+        add %edi, %edx   
+        add (%esi,%edx,4),%eax  #mettre le nombre dans elem
+        jmp incrementerC
+
+
+        incrementerC:
+        movl c,%edx
+        incl %edx
+        movl %edx, c
+        jmp Boucle2
+
+        incrementerR:
+        movl r, %edi
+        incl %edi
+        movl %edi, r
+        jmp Boucle1
+
+
+
+        
+   
+
+        retour:	
+        pop %esi
+        pop %ebx
+        pop %edi	
         leave          			/* Restore ebp and esp */
         ret           			/* Return to the caller */
 		
--- tp3/transpose.s
+++ tp3/transpose.s
@@ -1,3 +1,11 @@
+.data
+c: 
+     .int 0
+r:   
+     .int 0
+temp:
+     .int 0
+
 .globl matrix_transpose_asm
 
 matrix_transpose_asm:
@@ -5,6 +13,60 @@ matrix_transpose_asm:
         mov %esp, %ebp /* set ebp to current esp */
         
         /* Write your solution here */
+       push %edi
+       push %esi
+       
+       movl 8(%ebp),%esi     # le tableau inmatdata
+       movl 12(%ebp),%ebx    # le tableau  outmatdata
+       movl 16(%ebp), %edi   # mataorder        
+
+       Boucle1:
+       movl c, %ecx       # initialise C a chaque boucle 
+       movl $0,%ecx
+       movl %ecx, c
+       movl r, %eax
+       cmpl %eax, %edi    # comparer la valeur de r et le matorder
+       je retour
+       jmp Boucle2
+       
+
+       
+      Boucle2:
+        movl c, %ecx
+        cmpl %ecx, %edi    # comparer la valeur de C et matorder
+        je incrementerR            
+        movl r, %eax
+        imul  %edi,%eax
+        add %eax, %ecx         # mettre la position du nombre a mettre dans outmatdata dans %ecx
+        movl %ecx, temp
+        movl c, %ecx
+        movl r,%eax
+        imul %edi,%ecx
+        add %ecx, %eax          #mettre la posirtion du nombre a recuperer dans %eax
+
+
+
+        movl temp, %ecx
+        movl (%esi,%eax,4),%edx
+        movl %edx, (%ebx, %ecx, 4)    # mettre ce nombre dans le tableau outmatdata
+        jmp incrementerC
         
+        incrementerC:
+         movl c, %ecx
+         incl %ecx
+         mov %ecx, c
+         jmp Boucle2	
+
+        incrementerR:
+        mov r, %eax
+        incl %eax
+        movl %eax, r
+        jmp Boucle1
+       
+     
+        retour:
+        pop %edi
+        pop %esi
+        pop %esp
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
