80f71c50963adc426fb8783a06a6dace082b5f23
Edouard Hebert <edohebert12@gmail.com>
2017-04-18 21:05:46 -0400

    Fin tp5

--- tp5/tp5.c
+++ tp5/tp5.c
@@ -5,16 +5,9 @@ unsigned int Decryption_fct(unsigned int le)
 {
 	unsigned int be;
 
-
-	/*
-	 * Remplacez le code suivant par de l'assembleur en ligne
-	 * en utilisant le moins d'instructions possible
-	 */
 	// be = (le & 0xff000000) | (le & 0xff) << 16  | (le & 0xff00) | (le & 0xff0000) >> 16;
 
-
-
-	asm volatile (
+asm volatile (
 		"movl %%eax, %%ecx;\n\t"
 		"andl $0xff00ff00, %%ecx;\n\t"
 		"movl %%eax, %%edx;\n\t"
